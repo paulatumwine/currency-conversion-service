802544a2ae88e60fb0db85b946fc8062c383a968
Paul A. Tumwine <paulatumwine@gmail.com>
2020-04-08 00:21:35 -0500

    Add registration with Eureka server

--- src/main/java/com/samples/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/samples/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.samples.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients("com.samples.microservices.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
